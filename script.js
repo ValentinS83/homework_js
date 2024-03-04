94e49e4ebb7eeeb2be29363059d0684d31bbb56a
ValentinS83 <valentinstefan77@yahoo.com>
2024-03-04 21:02:37 +0200

    Add exercise 5

--- script.js
+++ script.js
@@ -10,3 +10,14 @@ console.log(`Rezultatul sumei dintre ${firstNumber} si ${secondNumber} este ${su
 let celsiusTemperature = 30;
 let fahrenheitTemperature = (celsiusTemperature * 9/5) + 32;
 console.log(fahrenheitTemperature);
+// ex 4
+let greeting = 'Hello ';
+let myName = 'Valentin';
+let fullGreeting = greeting + myName;
+console.log(fullGreeting);
+// ex 5
+let checkedNumber = 21;
+
+let reminder = checkedNumber % 2;
+let isEven = reminder == 0;
+console.log (`Number ${checkedNumber} is even: ${isEven}`);
