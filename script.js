c903edeb6b943b3041f200c73ad7944ae0b2c2a1
ValentinS83 <valentinstefan77@yahoo.com>
2024-03-04 20:32:46 +0200

    Homework exercise 2

---
+++ script.js
@@ -0,0 +1,7 @@
+let age = 41;
+console.log(age);
+
+let firstNumber = 10;
+let secondNumber = 5;
+let sum = firstNumber + secondNumber;
+console.log(`Rezultatul sumei dintre ${firstNumber} si ${secondNumber} este ${sum}`);
