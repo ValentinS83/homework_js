bac410f8b518e07a9729c014a8ee6100b2c110b0
ValentinS83 <valentinstefan77@yahoo.com>
2024-03-04 20:51:59 +0200

    Add exercise 3

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
+//ex 1
 let age = 41;
-console.log(age);
-
+console.log(`Varsta mea este ${age}`);
+// ex 2
 let firstNumber = 10;
 let secondNumber = 5;
 let sum = firstNumber + secondNumber;
 console.log(`Rezultatul sumei dintre ${firstNumber} si ${secondNumber} este ${sum}`);
+// ex 3
+let celsiusTemperature = 30;
+let fahrenheitTemperature = (celsiusTemperature * 9/5) + 32;
+console.log(fahrenheitTemperature);
